e9cb04059d6e5762aa106171eee2c69db1e22150
CSHJ0711 <csphj0711@naver.com>
2024-06-10 23:12:17 +0900

    지도삭제, 코드최적화

--- app/src/main/java/com/example/iot3/MainActivity.kt
+++ app/src/main/java/com/example/iot3/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.iot3
 import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.*
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -16,6 +19,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.util.*
+import android.location.Location
+import com.google.android.gms.maps.MapView
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "BluetoothArduino"
@@ -35,6 +40,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mpu6050_z: TextView
     private lateinit var connectButton: Button
     private lateinit var img_BH1750: ImageView
+    private lateinit var tv_warning: TextView
+
+    private val LOCATION_UPDATE_INTERVAL = 1000L
+    private val LOCATION_MIN_DISTANCE = 1f
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+
+
 
     companion object {
         const val MESSAGE_READ = 0
@@ -51,14 +65,36 @@ class MainActivity : AppCompatActivity() {
         mpu6050_z = findViewById(R.id.mpu6050_z)
         connectButton = findViewById(R.id.connectbutton)
         img_BH1750 = findViewById(R.id.img_BH1750)
+        tv_warning = findViewById(R.id.tv_warning)
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
         if (bluetoothAdapter == null) {
             Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show()
             finish()
             return
         }
-        connectToDevice()
+        connectToDevice() //앱 실행시 자동연결
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                // 시속을 얻어옴 (미터/초)
+                val speedValue = location.speed
+                // 미터/초를 km/시로 변환하여 정수형으로 변환
+                val kmPerHour = (speedValue * 3.6).toInt()
+                // 시속을 TextView에 표시
+                speed.text = "$kmPerHour km/h"
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+
+            //override fun onProviderEnabled(provider: String?) {}
+
+            //override fun onProviderDisabled(provider: String?) {}
+        }
+
+        handler = Handler(Looper.getMainLooper())
 
         connectButton.setOnClickListener {
             if (bluetoothGatt != null) {
@@ -86,11 +122,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    private fun connectToDevice() {
-        val device: BluetoothDevice = bluetoothAdapter.getRemoteDevice(DEVICE_ADDRESS)
-        bluetoothGatt = device.connectGatt(this, false, gattCallback)
     }
 
     private val gattCallback = object : BluetoothGattCallback() {
@@ -126,10 +157,14 @@ class MainActivity : AppCompatActivity() {
             processBluetoothMessage(message)
         }
     }
-
+    private fun connectToDevice() {
+        val device: BluetoothDevice = bluetoothAdapter.getRemoteDevice(DEVICE_ADDRESS)
+        bluetoothGatt = device.connectGatt(this, false, gattCallback)
+    }
     private fun disconnectFromDevice() {
         bluetoothGatt?.disconnect()
     }
+
 
     @SuppressLint("SetTextI18n")
     private fun processBluetoothMessage(message: String) {
@@ -138,25 +173,33 @@ class MainActivity : AppCompatActivity() {
         for (part in parts) {
             val keyValue = part.split(":") // 부분을 ":"를 기준으로 분할하여 키와 값으로 나눔
             if (keyValue.size == 2) {
-                val key = keyValue[0]
-                val value = keyValue[1]
+                val key = keyValue[0].trim()
+                val value = keyValue[1].trim()
 
-                when (key) {
-                    "speed" -> speed.text = value + "km/h"
-                    "bright" -> bright.text = value
-                    //if (value > "50") {
-                    //    img_BH1750.src =
-                    //}
-                    "ax" -> mpu6050_x.text = "x :$value"
-                    "ay" -> mpu6050_y.text = "y :$value"
-                    "az" -> mpu6050_z.text = "z :$value"
-                    // 다른 키에 대한 처리를 여기에 추가할 수 있음
+                runOnUiThread {
+                    when (key) {
+                        //"speed" -> speed.text = "$value km/h"
+                        "bright" -> bright.text = value
+                        "ax" -> mpu6050_x.text = "x: $value"
+                        "ay" -> mpu6050_y.text = "y: $value"
+                        "az" -> mpu6050_z.text = "z: $value"
+                        "warning:1" -> tv_warning.text = "넘어짐 감지됨!!"
+                        "light" -> {
+                            when (value) {
+                                "0" -> img_BH1750.setImageResource(R.drawable.bright_off)
+                                "1" -> img_BH1750.setImageResource(R.drawable.bright_on)
+                                // 다른 키에 대한 처리를 여기에 추가할 수 있음
+                            }
+                        }
+                    }
                 }
+                // Toast는 주석 처리했으므로 그대로 유지
+                // Logcat에 출력
+                Log.d(TAG, "$message")
             }
         }
-        Toast.makeText(this@MainActivity, "Received: $parts", Toast.LENGTH_SHORT).show()
-        Log.d(TAG, "$message")
     }
+
 
 
     override fun onDestroy() {
